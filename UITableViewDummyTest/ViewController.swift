688c45b163c2ebc14aa5e807bf6ef4638435c26c
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2021-03-16 14:48:04 +0900

    :bug: Added crash implementation

--- UITableViewDummyTest/ViewController.swift
+++ UITableViewDummyTest/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var initialLoad = false
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        tableView.beginUpdates()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        initialLoad = true
+        tableView.beginUpdates()
+
+        tableView.endUpdates()
+        tableView.endUpdates()
     }
 
 
 }
 
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return initialLoad ? 0 : 40
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return initialLoad ? 0 : 40
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
